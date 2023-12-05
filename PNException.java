99e56a188d8c1559412eaafd2f69da6c4159062a
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-04 22:22:30 -0300

    Create PNException.java

---
+++ PNException.java
@@ -0,0 +1,9 @@
+public class PNException extends RuntimeException {
+	
+	// Classe de exceção personalizada para produto 
+	// não encontrado no sistema.
+	public PNException(String message) {
+		super(message);
+	}
+	
+}
