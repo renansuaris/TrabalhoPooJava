32c9ab77722f073943453041987845af61d4e47b
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-04 22:18:45 -0300

    Create Alimentos.java

---
+++ Alimentos.java
@@ -0,0 +1,12 @@
+public final class Alimentos extends Produto {
+
+	public Alimentos(Integer id, String nome, Double preco, ProductsCategory categoria) {
+        super(id, nome, preco, categoria);
+    }
+    
+	@Override
+    public String toString() {
+    	return super.toString();
+    }
+	
+}
