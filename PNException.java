cf5084faefc1ab0bf7ca1b2a8e94f868ab065a70
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-09 16:27:25 -0300

    Delete PNException.java

--- PNException.java
+++
@@ -1,9 +0,0 @@
-public class PNException extends RuntimeException {
-	
-	// Classe de exceção personalizada para produto 
-	// não encontrado no sistema.
-	public PNException(String message) {
-		super(message);
-	}
-	
-}
