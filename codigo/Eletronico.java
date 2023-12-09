60be74dea5a2091e4f4e75bafb47a2c255350f14
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-09 16:19:34 -0300

    Create Eletronico.java

---
+++ codigo/Eletronico.java
@@ -0,0 +1,11 @@
+public final class Eletronico extends Produto {
+	
+	public Eletronico (Integer id, String nome, Double preco, ProductsCategory categoria) {
+        super(id, nome, preco, categoria);
+    }
+    
+	@Override
+    public String toString() {
+        return super.toString();
+    }
+}
