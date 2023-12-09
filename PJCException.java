3ab77cc893af0da5369822e266fdda0fee3a2e9d
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-09 16:27:19 -0300

    Delete PJCException.java

--- PJCException.java
+++
@@ -1,9 +0,0 @@
-public class PJCException extends RuntimeException {
-	
-	// Classe de exceção personalizada para produto
-	// já cadastrado no sistema.
-	public PJCException(String message) {
-		super(message);
-	}
-	
-}
