34b77a38a92e774424e0224e72aa155ea5c5e39e
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-09 16:21:50 -0300

    Create PJCException.java

---
+++ codigo/PJCException.java
@@ -0,0 +1,9 @@
+public class PJCException extends RuntimeException {
+	
+	// Classe de exceção personalizada para produto
+	// já cadastrado no sistema.
+	public PJCException(String message) {
+		super(message);
+	}
+	
+}
