de6bb772ed6040952ddd7e182e10803e55f6be31
Renan <112987845+renansuaris@users.noreply.github.com>
2023-12-09 16:21:18 -0300

    Create Moveis.java

---
+++ codigo/Moveis.java
@@ -0,0 +1,12 @@
+public final class Moveis extends Produto {
+
+	public Moveis(Integer id, String nome, Double preco, ProductsCategory categoria) {
+        super(id, nome, preco, categoria);
+    }
+    
+	@Override
+    public String toString() {
+    	return super.toString();
+    }
+
+}
